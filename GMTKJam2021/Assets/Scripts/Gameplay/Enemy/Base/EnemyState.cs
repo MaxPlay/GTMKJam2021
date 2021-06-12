cf05042f64df8db9bc655a8f7cb3b87f68666f7f
MaxPlay <maxplayyt@gmail.com>
2021-06-12 18:01:06 +0200

    Added navmesh to test scene for enemy Implemented enemy state machine  -> Can be extended for other enemy types  -> Buggy now has a prefab with logic attached

---
+++ GMTKJam2021/Assets/Scripts/Gameplay/Enemy/Base/EnemyState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace GMTK2021.Gameplay.Enemy.Base
+{
+    public abstract class EnemyState
+    {
+        public EnemyBehavior OwningBehavior { get; set; }
+
+        public Transform Transform => OwningBehavior.transform;
+
+        public GameObject GameObject => OwningBehavior.gameObject;
+
+        public T GetComponent<T>() where T : Component => OwningBehavior.GetComponent<T>();
+        public T GetComponentInChildren<T>() where T : Component => OwningBehavior.GetComponentInChildren<T>();
+
+        public T GetOwner<T>() where T : EnemyBehavior => OwningBehavior as T;
+
+        public void SetState<T>() where T : EnemyState => OwningBehavior.SetState<T>();
+
+        public abstract void Enter();
+        public abstract void Update();
+        public abstract void Exit();
+    }
+}
---
+++ GMTKJam2021/Assets/Scripts/Gameplay/Enemy/BuggyBehavior.cs
@@ -0,0 +1,40 @@
+﻿using GMTK2021.Gameplay.Enemy.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GMTK2021.Gameplay.Enemy
+{
+    public class BuggyBehavior : EnemyBehavior
+    {
+        [SerializeField]
+        private float distanceToPlayer;
+
+        public Vector3? TargetPosition { get; private set; }
+
+        protected override void Initialize()
+        {
+            AddState<BuggyIdleState>();
+            AddState<BuggyMoveState>();
+        }
+
+        public bool CheckForPlayer()
+        {
+            Vector3 playerLocation = GameManager.Player.transform.position;
+            if (Physics.Raycast(transform.position, playerLocation - transform.position, out RaycastHit hitInfo, distanceToPlayer))
+            {
+                if (hitInfo.transform.CompareTag("Player"))
+                {
+                    TargetPosition = hitInfo.point;
+                    return true;
+                }
+            }
+            if (NavMeshAgent.isStopped)
+                TargetPosition = null;
+            return false;
+        }
+    }
+}
