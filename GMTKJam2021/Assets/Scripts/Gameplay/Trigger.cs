ca7eef9737606faaefa492796670362d5bb1d3e6
MaxPlay <maxplayyt@gmail.com>
2021-06-13 18:27:47 +0200

    Minor lighting tweaks Added "Enemy" label to enemy prefabs Made Railings static and set layer to "Ignore Raycast" Improved speed of Door animations Implemented trigger logic for "trigger once" and "enabled" Fixed lighting issues in the render pipeline Built some level data Did some further scripting in level 1

--- GMTKJam2021/Assets/Scripts/Gameplay/Trigger.cs
+++ GMTKJam2021/Assets/Scripts/Gameplay/Trigger.cs
@@ -17,31 +17,70 @@ namespace GMTK2021.Gameplay
 
         private HashSet<GameObject> gameObjectsInTrigger = new HashSet<GameObject>();
 
+        [SerializeField]
+        private bool triggerOnce;
+
+        public bool Enabled
+        {
+            get => isEnabled;
+            set => isEnabled = value;
+        }
+
         public UnityEvent OnEnter;
         public UnityEvent OnExit;
+
+        [SerializeField]
+        private bool isEnabled = true;
 
         private void OnTriggerEnter(Collider other)
         {
             if (triggeredByTags.Count == 0 || triggeredByTags.Any(other.CompareTag))
             {
+                if (triggerOnce && isEnabled)
+                {
+                    Collider[] colliders = GetComponents<Collider>();
+                    for (int i = 0; i < colliders.Length; i++)
+                    {
+                        colliders[i].enabled = false;
+                    }
+                    TriggerEnter();
+                    TriggerExit();
+                    return;
+                }
+
                 if (!gameObjectsInTrigger.Contains(other.gameObject))
                 {
                     gameObjectsInTrigger.Add(other.gameObject);
-                    OnEnter.Invoke();
+                    TriggerEnter();
                 }
             }
         }
 
+        private void TriggerEnter()
+        {
+            if (isEnabled)
+                OnEnter.Invoke();
+        }
+
         private void OnTriggerExit(Collider other)
         {
+            if (triggerOnce)
+                return;
+
             if (triggeredByTags.Count == 0 || triggeredByTags.Any(other.CompareTag))
             {
                 if (gameObjectsInTrigger.Contains(other.gameObject))
                 {
                     gameObjectsInTrigger.Remove(other.gameObject);
-                    OnExit.Invoke();
+                    TriggerExit();
                 }
             }
+        }
+
+        private void TriggerExit()
+        {
+            if (isEnabled)
+                OnExit.Invoke();
         }
 
         private void OnDrawGizmos()
